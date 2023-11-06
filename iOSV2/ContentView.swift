2eacc2bc20854ed5f262e3cd075e44a9df006495
Marcus921 <93766924+Marcus921@users.noreply.github.com>
2023-11-06 10:26:02 +0100

    Fixa text

--- iOSV2/ContentView.swift
+++ iOSV2/ContentView.swift
@@ -14,6 +14,8 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hurra, Version 1!")
+            
+            Text("Botten")
         }
         .padding()
     }
