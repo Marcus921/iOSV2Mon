1822abd4269d62a07c8c04b6e3c9e4df1dfa3923
Marcus921 <93766924+Marcus921@users.noreply.github.com>
2023-11-06 09:57:59 +0100

    V1 Git Test

--- iOSV2/ContentView.swift
+++ iOSV2/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, worlmmd!")
+            Text("Hurra, V1!")
         }
         .padding()
     }
