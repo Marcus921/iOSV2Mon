25757a6f9eab6b356b6193e477685f622be92885
Marcus921 <93766924+Marcus921@users.noreply.github.com>
2023-11-06 09:08:42 +0100

    Initial Commit

---
+++ iOSV2/iOSV2App.swift
@@ -0,0 +1,17 @@
+//
+//  iOSV2App.swift
+//  iOSV2
+//
+//  Created by Marcus Malmgren on 2023-11-06.
+//
+
+import SwiftUI
+
+@main
+struct iOSV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
